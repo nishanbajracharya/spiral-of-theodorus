0f7a093cda37f72a165c2c5e73c930e9186adc81
Nishan Bajracharya <nisbaj11@gmail.com>
2024-06-17 20:48:06 +0545

    Initial canvas setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import './style.css';
-
 import 'normalize.css';
+
+import './style.css';
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d')!;
+canvas.width = window.innerWidth * .8;
+canvas.height = window.innerHeight * .8;
+
+const app = document.querySelector('#app')!;
+
+app.appendChild(canvas);
+
+function draw(time: number) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  requestAnimationFrame(draw);
+};
+
+requestAnimationFrame(draw);
