38e8fb4820d58c3ff4d6b10842a486b4dfdd0ebf
Nishan Bajracharya <nisbaj11@gmail.com>
2024-06-17 22:05:10 +0545

    Updates

--- js/index-265K5QVG.js
+++
@@ -1,2 +0,0 @@
-(()=>{var e=document.createElement("canvas"),n=e.getContext("2d");e.width=window.innerWidth*.8;e.height=window.innerHeight*.8;var o=document.querySelector("#app");o.appendChild(e);function t(i){n.clearRect(0,0,e.width,e.height),requestAnimationFrame(t)}requestAnimationFrame(t);})();
-//# sourceMappingURL=index-265K5QVG.js.map
